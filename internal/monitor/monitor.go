f2caf6b2c07081d8f545ca4a60f20dd09b105ca8
Kenny Y <24802984+kenny-y-dev@users.noreply.github.com>
2023-06-06 18:20:57 -0400

    Adjust SendPing to return reference to ping object. Add additional tests

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -7,22 +7,22 @@ import (
 	"github.com/go-ping/ping"
 )
 
-func SendPing(target string, timeout string) (ping.Pinger, error) {
+func SendPing(target string, timeout string) (*ping.Pinger, error) {
 	pinger, err := ping.NewPinger(target)
 	if err != nil {
-		return *pinger, fmt.Errorf("Pinger creation failed with error: %w", err)
+		return pinger, fmt.Errorf("Pinger creation failed with error: %w", err)
 	}
 
 	pinger.Count = 3
 	pinger.Timeout, err = time.ParseDuration(timeout)
 	if err != nil {
-		return *pinger, fmt.Errorf("Timeout parsing failed with error: %w", err)
+		return pinger, fmt.Errorf("Timeout parsing failed with error: %w", err)
 	}
 	err = pinger.Run()
 	if err != nil {
-		return *pinger, fmt.Errorf("Unable to run ping: %w", err)
+		return pinger, fmt.Errorf("Unable to run ping: %w", err)
 	}
-	return *pinger, nil
+	return pinger, nil
 }
 
 func CheckSuccessPing(stats ping.Statistics, strict bool) bool {
--- internal/monitor/monitor_test.go
+++ internal/monitor/monitor_test.go
@@ -6,6 +6,37 @@ import (
 	"github.com/go-ping/ping"
 )
 
+func TestCheckPingSuccess(t *testing.T) {
+	want := 3
+
+	pinger, err := SendPing("127.0.0.1", "5s")
+	if err != nil {
+		t.Errorf("Error sending ping: %v", err)
+	}
+	if want != pinger.Statistics().PacketsRecv {
+		t.Errorf("Ping module failed to receive 3 packets")
+	}
+}
+
+func TestCheckPingFailure(t *testing.T) {
+	want := 0
+	pinger, err := SendPing("203.0.113.1", "1s") // IP address should not route per RFC 5737
+	if err != nil {
+		t.Errorf("Ping module failed with error: %v", err)
+	}
+	if want != pinger.Statistics().PacketsRecv {
+		t.Errorf("Ping module received reply packets when it shouldn't have")
+	}
+
+}
+
+func TestCheckPingError(t *testing.T) {
+	pinger, err := SendPing("127.0.0.1", "5invalid")
+	if pinger != nil && err == nil {
+		t.Errorf("Ping module did not throw error on invalid timeout value")
+	}
+
+}
 func TestCheckSuccess(t *testing.T) {
 	testStats := ping.Statistics{
 		PacketsRecv: 2,
@@ -17,7 +48,7 @@ func TestCheckSuccess(t *testing.T) {
 	if CheckSuccessPing(testStats, true) {
 		t.Errorf("Expected CheckSuccess to return False, returned True")
 	}
-	*&testStats.PacketsRecv = 0
+	testStats.PacketsRecv = 0
 	if CheckSuccessPing(testStats, false) {
 		t.Errorf("Expected CheckSuccess to return False, returned True")
 	}
