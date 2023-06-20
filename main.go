2f198ff8e1fd82b946c287d4f2ea486138b7af07
Kenny Y <24802984+kenny-y-dev@users.noreply.github.com>
2023-06-19 22:49:27 -0400

    Split out checker function in main.go, pass in ref to status
    Allows for status to be set in main setup, a bit more modular and can change if target should be default up/down in the future with config
    Status gets passed in as a pointer, so function will adjust the bool declared in main instead of a copy in checkStatus

--- main.go
+++ main.go
@@ -9,25 +9,31 @@ import (
 )
 
 func main() {
-	status := true
 	cfg := config.Build()
+	status := true
+	for {
+		checkStatus(cfg, &status)
+	}
+}
+
+func checkStatus(cfg config.MonitorConfig, status *bool) {
 	pinger, err := monitor.SendPing(cfg.MonitorTarget, cfg.MonitorTimeout)
 	if err != nil {
 		log.Fatalf("Ping module failed %v", err)
 	}
 	up := monitor.CheckSuccessPing(*pinger.Statistics(), cfg.MonitorCheckStrict)
-	if up != status {
-		status = !status
-		if !status {
+	if up != *status {
+		*status = !*status
+		if !*status {
 			log.Printf("Target failed monitor check")
-			res, err := notify.HttpReq(cfg.NotifyMethod, cfg.NotifyTarget, nil)
+			res, err := notify.HttpReq(cfg.NotifyMethod, cfg.NotifyTarget, cfg.NotifyDownJSON)
 			if err != nil {
 				log.Printf("Failed to notify target down with error: %v", err)
 			}
 			log.Printf("Sent Down notification to target, return code: %v", res.StatusCode)
 			// target down
 		}
-		if status {
+		if *status {
 			log.Printf("Target healthy")
 			res, err := notify.HttpReq(cfg.NotifyMethod, cfg.NotifyTarget, cfg.NotifyUpJSON)
 			if err != nil {
